8063129bd59e8f3467afa5acd8c432cc063e4dc4
Alessandro Godino <gdnlsn04d28m208n@studenti.unical.it>
2026-07-17 11:56:31 +0200

    minor changes

--- hasta-frontend/src/app/app.routes.ts
+++ hasta-frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Home } from './features/home/home';
 import { AuctionDetail } from './features/auction-detail/auction-detail';
 import { CategoryPage } from './features/category-page/category-page';
 import { UserProfile } from './features/user-profile/user-profile';
-import { Settings } from './features/settings/settings';
 import { Login } from './features/login/login';
 import { authGuard } from './core/guards/auth.guard';
 import { Register } from './features/register/register';
@@ -14,7 +13,6 @@ export const routes: Routes = [
   { path: 'aste/:id', component: AuctionDetail },
   { path: 'categoria/:id', component: CategoryPage },
   { path: 'utente', component: UserProfile, canActivate: [authGuard] },
-  { path: 'impostazioni', component: Settings, canActivate: [authGuard] },
   { path: 'vendi', component: Sell, canActivate: [authGuard] },
   { path: 'login', component: Login },
   { path: 'registrati', component: Register },
